60a6fb1a9d077b3ccb0ba31a84c37788d341334b
riyadali <riyadali@gmail.com>
2018-11-15 15:04:18 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
 // https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
 // For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
-mongoose.plugin(schema => { schema.options.usePushEach = true });
+// mongoose.plugin(schema => { schema.options.usePushEach = true }); --- didn't work
 
 var uniqueValidator = require('mongoose-unique-validator');
 var crypto = require('crypto');
@@ -20,8 +20,12 @@ var UserSchema = new mongoose.Schema({
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   hash: String,
   salt: String
-}, {timestamps: true});
-
+// Getting an error with mongoose for unknown modifier for $pushAll
+// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
+// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
+// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
+// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
+}, {timestamps: true, usePushEach: true});
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 UserSchema.methods.validPassword = function(password) {
