95f95472a5ff26eba79a31039fb84e02a4abb1ed
riyadali <riyadali@gmail.com>
2018-11-15 14:41:38 -0500

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,10 @@
 var mongoose = require('mongoose');
+// Getting an error with mongoose for unknown modifier for $pushAll
+// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
+// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
+// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
+// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
+mongoose.plugin(schema => { schema.options.usePushEach = true });
 
 var CommentSchema = new mongoose.Schema({
   body: String,
