e990dfe5fbb40c8e5834c9435b12059fe983a0a1
riyadali <riyadali@gmail.com>
2018-11-15 20:38:27 -0500

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -1,10 +1,4 @@
 var mongoose = require('mongoose');
-// Getting an error with mongoose for unknown modifier for $pushAll
-// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
-// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
-// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
-// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
-// mongoose.plugin(schema => { schema.options.usePushEach = true }); --- didn't work
 var uniqueValidator = require('mongoose-unique-validator');
 var slug = require('slug');
 var User = mongoose.model('User');
@@ -18,12 +12,7 @@ var ArticleSchema = new mongoose.Schema({
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   tagList: [{ type: String }],
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-// Getting an error with mongoose for unknown modifier for $pushAll
-// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
-// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
-// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
-// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
-}, {timestamps: true, usePushEach: true});
+}, {timestamps: true});
 
 ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
 
