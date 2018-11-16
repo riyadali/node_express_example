dc5adbd63fc726be9cf9525480e6eb05780a583a
riyadali <riyadali@gmail.com>
2018-11-15 20:40:02 -0500

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,21 +1,10 @@
 var mongoose = require('mongoose');
-// Getting an error with mongoose for unknown modifier for $pushAll
-// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
-// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
-// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
-// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
-// mongoose.plugin(schema => { schema.options.usePushEach = true }); --- didn't work
 
 var CommentSchema = new mongoose.Schema({
   body: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' }
-// Getting an error with mongoose for unknown modifier for $pushAll
-// Apparently this problem goes away if you move to mongoose version 5. I couldn't do this because the Yarn
-// lockfile prevented me from upgrading mongoose. COuld do this at home at some point (refer to 
-// https://medium.com/@drdmason/how-i-resolve-conflicts-in-yarn-lock-7083331b5969
-// For now I'm using a workaround recommended here https://github.com/Automattic/mongoose/issues/5924
-}, {timestamps: true, usePushEach: true});
+}, {timestamps: true});
 
 // Requires population of author
 CommentSchema.methods.toJSONFor = function(user){
