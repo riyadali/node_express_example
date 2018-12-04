dd1b36554cb6a1831e364da35c65da7f48c94fd5
riyadali <riyadali@gmail.com>
2018-12-04 12:40:23 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
   // original -- username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9\S]+$/, 'is invalid'], index: true},
+  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9\s]+$/, 'is invalid'], index: true},
   email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
   bio: String,
   image: String,
