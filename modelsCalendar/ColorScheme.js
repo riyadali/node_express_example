4c78f5ba7907fdad1a0a57a40610d3af728b6074
riyadali <riyadali@gmail.com>
2019-01-22 15:46:01 -0500

    Update ColorScheme.js

--- modelsCalendar/ColorScheme.js
+++ modelsCalendar/ColorScheme.js
@@ -1,19 +1,39 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var slug = require('slug');
 
-var CommentSchema = new mongoose.Schema({
-  body: String,
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' }
+var ColorSchemeSchema = new mongoose.Schema({
+  slug: {type: String, lowercase: true, unique: true},
+  name: String,
+  primary: String,
+  secondary: String,
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, {timestamps: true});
 
-// Requires population of author
-CommentSchema.methods.toJSONFor = function(user){
+ColorSchemeSchema.plugin(uniqueValidator, {message: 'is already taken'});
+
+ColorSchemeSchema.pre('validate', function(next){
+  if(!this.slug)  {
+    this.slugify();
+  }
+
+  next();
+});
+
+ColorSchemeSchema.methods.slugify = function() {
+  this.slug = slug(this.name) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
+};
+
+// Requires population of owner
+ColorSchemeSchema.methods.toJSONFor = function(user){
   return {
     id: this._id,
-    body: this.body,
-    createdAt: this.createdAt,
-    author: this.author.toProfileJSONFor(user)
+    slug: this.slug,
+    name: this.name,
+    primary: this.primary,
+    secondary: this.secondary,    
+    owner: this.owner.toProfileJSONFor(user)
   };
 };
 
-mongoose.model('Comment', CommentSchema);
+mongoose.model('ColorScheme', ColorSchemeSchema);
