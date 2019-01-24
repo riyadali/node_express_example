87e79f0ee329390a3ca63f6ed26929ed606364d4
riyadali <riyadali@gmail.com>
2019-01-24 13:45:03 -0500

    Update colorSchemes.js

--- routesCalendar/api/colorSchemes.js
+++ routesCalendar/api/colorSchemes.js
@@ -18,14 +18,20 @@ router.param('colorScheme', function(req, res, next, slug) {
 });
 
 // return a colorScheme
+// I think auth should be required for this one
+// you should only be allowed to access color schemes you own or
+// the "default" color schemes (with no owner field)
 router.get('/:colorScheme', auth.optional, function(req, res, next) {
   Promise.all([
     req.payload ? User.findById(req.payload.id) : null,
     req.colorScheme.populate('owner').execPopulate()
   ]).then(function(results){
     var user = results[0];
-
-    return res.json({colorScheme: req.colorScheme.toJSONFor(user)});
+    
+    if (user) 
+      return res.json({colorScheme: req.colorScheme.toJSONFor(user)});
+    else
+      return res.json({colorScheme: req.colorScheme.toJSON()});
   }).catch(next);
 });
 
@@ -41,6 +47,50 @@ router.post('/', auth.required, function(req, res, next) {
       console.log(colorScheme.owner);
       return res.json({colorScheme: colorScheme.toJSONFor(user)});
     });
+  }).catch(next);
+});
+
+// update article
+router.put('/:article', auth.required, function(req, res, next) {
+  User.findById(req.payload.id).then(function(user){
+    if(req.article.author._id.toString() === req.payload.id.toString()){
+      if(typeof req.body.article.title !== 'undefined'){
+        req.article.title = req.body.article.title;
+      }
+
+      if(typeof req.body.article.description !== 'undefined'){
+        req.article.description = req.body.article.description;
+      }
+
+      if(typeof req.body.article.body !== 'undefined'){
+        req.article.body = req.body.article.body;
+      }
+
+      if(typeof req.body.article.tagList !== 'undefined'){
+        req.article.tagList = req.body.article.tagList
+      }
+
+      req.article.save().then(function(article){
+        return res.json({article: article.toJSONFor(user)});
+      }).catch(next);
+    } else {
+      return res.sendStatus(403);
+    }
+  });
+});
+
+// delete article
+router.delete('/:article', auth.required, function(req, res, next) {
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    if(req.article.author._id.toString() === req.payload.id.toString()){
+      return req.article.remove().then(function(){
+        return res.sendStatus(204);
+      });
+    } else {
+      return res.sendStatus(403);
+    }
   }).catch(next);
 });
 
